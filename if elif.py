8a1eec1e1d6dab2e6505693d3409e6b59aacf5bc
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2022-10-11 20:24:04 +0530

    Add files via upload

---
+++ if elif.py
@@ -0,0 +1,18 @@
+
+a=33
+b=333
+if b>a:
+    print("b is greater than a")
+elif a==b:
+    print("a is equal to b")
+else:
+    print("a is greater than b")
+    
+a=200
+b=33
+if a>b: print("a is greater than b")
+
+a=200
+b=33
+print("a") if a>b else print("=") if a==b else print("b")
+
