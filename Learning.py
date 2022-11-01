58254103bbcf0c941f6e2a752e507dbe9d39b76c
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2022-11-01 08:42:13 +0530

    Add files via upload

---
+++ Learning.py
@@ -0,0 +1,11 @@
+movies_dict = {
+    'title': 'Aviator',
+    'year': '2005',
+    'demo': False,
+    'director': 'Martin Scorsese',
+    'distributor': 'Miramax Films'
+}
+
+
+def director():
+    return movies_dict[]
